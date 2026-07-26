e676ff1fa942b4c2e2b0657b110799c7b6dcec99
Andrei Zotin <hapice2@gmail.com>
2026-07-26 20:20:21 +0300

    chore: add vitest, @vue/test-utils and jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -16,6 +15,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{ts,tsx,js,jsx}']
   },
   css: {
     preprocessorOptions: {
